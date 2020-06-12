f8c6e01c2409a48f2a808cf54cfc86a15f303126
Victor Samuel <hola@victorsamuel.com>
2020-06-11 23:49:27 -0500

    feature: new !ivano command

---
+++ src/actions/ivanoInvoke.ts
@@ -0,0 +1,9 @@
+import { Message } from 'discord.js';
+
+const ivanoId = '531705363804848129';
+
+export const ivanoInvoke = (message: Message) => {
+  message.reply(
+    `<@${ivanoId}>: Pinche Ivano vicio, te pasas de verga, ya deja de jugar un rato, siempre te la pasas de pinche vicio, alv.`
+  );
+};
