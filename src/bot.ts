00926bbbf65731e04e884b540c969452ffefa117
Victor Samuel <hola@victorsamuel.com>
2020-06-11 23:53:37 -0500

    refactor: better entrypoint

--- src/bot.ts
+++ src/bot.ts
@@ -3,15 +3,9 @@ require('dotenv').config();
 import { Client } from 'discord.js';
 import { messageListener } from './listeners/message.listener';
 
-const TOKEN = process.env.TOKEN;
 const discordBot = new Client();
 
-discordBot
-  .login(TOKEN)
-  .then()
-  .catch((reason) => {
-    console.error(reason);
-  });
+discordBot.login(process.env.TOKEN).then().catch(console.error);
 
 discordBot.on('ready', () => console.info(`Logged in as ${discordBot.user.tag}!\n`));
 discordBot.on('message', messageListener);
